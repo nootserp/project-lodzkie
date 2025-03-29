60c14ffb1797af5901e0a5b44958eb8f07cf5db6
nootserp <filciu990@gmail.com>
2025-03-29 19:45:41 +0100

    Update autofarma-v2.lua

--- autofarma-v2.lua
+++ autofarma-v2.lua
@@ -68,7 +68,7 @@ local function v(w)
     end
     y.CFrame = CFrame.new(y.Position, j.Parent.Position)
     fireproximityprompt(j)
-    task.wait(1.5)
+    task.wait()
     local z = m()
     if not z then
         return nil
